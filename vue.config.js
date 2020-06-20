f5eb52306e2820e860819daab92dc8938066427e
钟钱胜 <7635692+zhongqiansheng@user.noreply.gitee.com>
2020-06-21 00:18:50 +0800

    路径修改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/myresume/' : './',
+    publicPath: process.env.NODE_ENV === 'production' ? '/MyResume/dist/' : './',
     productionSourceMap: false,
 }
