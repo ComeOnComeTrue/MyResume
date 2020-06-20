12baa2829037c65f29f15a5ecde0b3df09118b71
钟钱胜 <7635692+zhongqiansheng@user.noreply.gitee.com>
2020-06-21 00:24:41 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import $ from 'jquery'
+import $ from 'jquery'
 
 import "./assets/reset.css" // 初始化css
 import "./assets/font.css" // iconfont 图标
@@ -12,7 +12,7 @@ import "velocity-animate" // 动画插件
 // import "element-ui/lib/theme-chalk/index.css"
 // Vue.use(ElementUI)
 
-// Vue.prototype.$ = $; // jquery
+Vue.prototype.$ = $; // jquery
 
 Vue.config.productionTip = false
 
